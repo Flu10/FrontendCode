e5c681375417ba24c9b7686ca20d624d0fec3e0d
Flu10.marian_fluturas@yahoo.com <Flu10.marian_fluturas@yahoo.com>
2016-06-23 11:26:56 +0300

    Workshop day1 (demo ex)

---
+++ Day 1/scripts/app.js
@@ -0,0 +1,41 @@
+var employeesList = [
+    {
+        firstName: 'John',
+        lastName: 'King',
+        phone: '0123456789',
+        salary: 4500
+    },
+    {
+        firstName: 'Steven',
+        lastName: 'Gerard',
+        phone: '0123456789',
+        salary: 4500
+    },
+    {
+        firstName: 'Diana',
+        lastName: 'Ross',
+        phone: '0123456789',
+        salary: 4500
+    },
+    {
+        firstName: 'Mike',
+        lastName: 'Bob',
+        phone: '0123456789',
+        salary: 4500
+    },
+    {
+        firstName: 'Emily',
+        lastName: 'Hudson',
+        phone: '0123456789',
+        salary: 4500
+    }
+];
+function showList() {
+    var myTable = '<table border="1"><tr><th>First Name</th><th>Last Name</th><th>Phone</th><th>Salary</th></tr>';
+    for(var i in employeesList) {
+        myTable += '<tr><td>'+employeesList[i].firstName+'</td><td>'+employeesList[i].lastName+'</td><td>'+employeesList[i].phone+'</td><td>'+employeesList[i].salary+'</td></tr>';
+    }
+    myTable += '</table>';
+    var container = document.getElementById('listcontainer');
+    container.innerHTML = myTable;
+}
