82c5aa06181d05fcae2cef50dfc2a2e6e0c0676f
Fluturas Marian <Fluturas Marian>
2016-06-26 10:00:14 +0300

    Last version

--- Day 1/scripts/app.js
+++ Day 1/scripts/app.js
@@ -31,11 +31,44 @@ var employeesList = [
     }
 ];
 function showList() {
-    var myTable = '<table border="1"><tr><th>First Name</th><th>Last Name</th><th>Phone</th><th>Salary</th></tr>';
+    var myTable = '<table class="table table-hover" id="MTable" border="1"><tr><th>First Name</th><th>Last Name</th><th>Phone</th><th id="myTd">Salary</th></tr>';
     for(var i in employeesList) {
-        myTable += '<tr><td>'+employeesList[i].firstName+'</td><td>'+employeesList[i].lastName+'</td><td>'+employeesList[i].phone+'</td><td>'+employeesList[i].salary+'</td></tr>';
+        myTable += '<tr><td>'+employeesList[i].firstName+'</td><td>'+employeesList[i].lastName+'</td><td>'
+            +employeesList[i].phone+'</td><td >'+employeesList[i].salary+'</td></tr>';
     }
     myTable += '</table>';
     var container = document.getElementById('listcontainer');
     container.innerHTML = myTable;
+}
+function addEmployee() {
+    var table = document.getElementById('MTable');
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var firstN= document.getElementById('firstName').value;
+    var lastN= document.getElementById('lastName').value;
+    var fhone= document.getElementById('phone').value;
+    var salaryy= document.getElementById('salary').value;
+    cell1.innerHTML =firstN;
+cell2.innerHTML = lastN;
+cell3.innerHTML = fhone;
+cell4.innerHTML = salaryy;
+}
+function SalaryT(){
+
+    var Salary= 0 ;
+
+    var table = document.getElementById('MTable');
+
+    for( var i=0;i<x;i++){
+        Salary=document.table.rows[i].cell(3).value+Salary;
+    }//NU VREAAAA SA MEARGA !!!!!!!!
+document.write(i);
+
+}
+function deleteEmployee() {
+    document.getElementById("MTable").deleteRow(1);
+
 }
